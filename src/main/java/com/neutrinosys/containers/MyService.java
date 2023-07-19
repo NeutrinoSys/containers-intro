5f0edb70251cf777303fb923787e8205b5080649
Terry Martin <tcmartin24@gmail.com>
2023-07-18 21:23:05 -0500

    Completion of 'Setter Injection' lesson.

--- src/main/java/com/neutrinosys/containers/MyService.java
+++ src/main/java/com/neutrinosys/containers/MyService.java
@@ -1,6 +1,5 @@
 package com.neutrinosys.containers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -8,11 +7,16 @@ import javax.inject.Inject;
 @Service
 public class MyService {
 
-    @Inject
     private MyRepository myRepository;
 
     public void process(Object object) {
         System.out.println("Processing... " + object);
         myRepository.save(object.toString().toUpperCase());
+    }
+
+    @Inject
+    public void setMyRepository(MyRepository myRepository) {
+        this.myRepository = myRepository;
+        System.out.println("Received the repository...");
     }
 }
